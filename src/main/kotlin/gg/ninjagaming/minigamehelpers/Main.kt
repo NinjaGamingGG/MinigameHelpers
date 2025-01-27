dcb97ea13ff7005a58124bbc0f5c90201f39ef6f
Taschner <admin@NinjaGaming.gg>
2025-01-27 14:29:23 +0100

    Initial commit

---
+++ src/main/kotlin/gg/ninjagaming/minigamehelpers/Main.kt
@@ -0,0 +1,20 @@
+package gg.ninjagaming.minigamehelpers
+
+/**
+ * The entry point of the Kotlin application.
+ *
+ * This function serves as the starting point of the program
+ * and is automatically invoked when the application is run.
+ * Place the application logic or function calls within this method
+ * to execute the desired behavior.
+ */
+fun main() {
+}
+
+/**
+ * Main class serving as the entry point of the application.
+ *
+ * Contains the main functionality to initiate the program.
+ */
+class Main (){
+}
