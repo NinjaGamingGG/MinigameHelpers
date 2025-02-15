031037e9f6bdd1c3f7a2b9e508c5c9cb8f6644d8
Taschner <admin@NinjaGaming.gg>
2025-02-15 09:29:12 +0100

    Add PostGameRunnable for post-game countdown logic
    This adds a runnable to handle actions after the game ends, including displaying a title with the winner's name (if available) and a countdown until the server shuts down. It ensures smoother transitions and clearer communication for players post-game.

---
+++ src/main/kotlin/gg/ninjagaming/minigamehelpers/commonRunnables/PostGameRunnable.kt
@@ -0,0 +1,40 @@
+package gg.ninjagaming.minigamehelpers.commonRunnables
+
+import gg.ninjagaming.minigamehelpers.commonHelpers.ArenaHelper
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.Title.Times.times
+import net.kyori.adventure.title.Title.title
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import java.time.Duration
+import kotlin.collections.forEach
+
+object PostGameRunnable {
+    fun scheduleRunnable(winningPlayer: Player?, waitingTime: Int): Runnable {
+        val runnable = Runnable {
+            mainGameTick(winningPlayer, waitingTime)
+        }
+
+        return runnable
+
+    }
+
+    fun mainGameTick(winningPlayer: Player?, waitingTime: Int){
+        var countdown = waitingTime
+        if (countdown == 0)
+            Bukkit.shutdown()
+
+
+        var mainTitle = Component.text("No player has won the game.")
+        if (winningPlayer != null)
+            mainTitle = Component.text("Player §6${winningPlayer.name}§r has won the game!")
+
+        val winnerTitle = title(mainTitle, Component.text("§oServer Stops in $countdown seconds"), times(Duration.ZERO, Duration.ofSeconds(3), Duration.ZERO))
+        val players = ArenaHelper.PlayerManagement.getPlayerList()
+        players.forEach {
+            it.showTitle(winnerTitle)
+        }
+
+        countdown--
+    }
+}
